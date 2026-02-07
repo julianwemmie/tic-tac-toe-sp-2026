dbbba8c5fdd208e1d5ca4e288a9bc7fbe956e565
julianwemmie <julianwemmie@gmail.com>
2026-02-07 09:30:26 -0500

    add env vars for protocol and host

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { type SocketRequest, type SocketResponse } from './types/ws';
 import Lobby from './components/Lobby';
 import type { Room as RoomType } from './types/server';
 import Room from './components/Room';
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'ws://localhost:3000'
+const VITE_API_PORT = import.meta.env.VITE_API_PORT || '3000'
+const VITE_API_HOST = import.meta.env.VITE_API_HOST || 'localhost'
+const VITE_API_USE_HTTPS = import.meta.env.VITE_API_USE_HTTPS === 'true'
 
+// const HTTP_BASE_URL = `${VITE_API_USE_HTTPS ? 'https' : 'http'}://${VITE_API_HOST}:${VITE_API_PORT}`
+const WS_BASE_URL = `${VITE_API_USE_HTTPS ? 'wss' : 'ws'}://${VITE_API_HOST}:${VITE_API_PORT}`
 
 function App() {
 
@@ -18,7 +22,7 @@ function App() {
   }
 
   useEffect(() => {
-    const ws = new WebSocket(BASE_URL + '/ws')
+    const ws = new WebSocket(WS_BASE_URL + '/ws')
     ws.onopen = () => {
       wsRef.current = ws
     }
@@ -44,7 +48,7 @@ function App() {
         justifyContent: 'center',
         alignItems: 'center'
       }}>
-        {/* <button onClick={() => fetch('http://localhost:3000/debug')}>debug</button> */}
+        {/* <button onClick={() => fetch(`${HTTP_BASE_URL}/debug`)}>debug</button> */}
         <h3>Ultimate Tic-Tac-Toe!</h3>
         {room ? (
           <Room
